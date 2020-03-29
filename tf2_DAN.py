4623a845686e16e4b67b9502e84b04a8ea5e7314
orangeqqq <Zlife_0@163.com>
2020-03-29 11:51:10 +0800

    GPU

--- tf2_DAN.py
+++ tf2_DAN.py
@@ -6,14 +6,44 @@ import time
 import pandas 
 import logging
 import functools
+import os
+from datetime import datetime
+#只使用CPU
+#os.environ["CUDA_VISIBLE_DEVICES"] = "-1"  # ='-1' 设置gpu不可见
+
+#设置GPU设备使用
+gpus = tf.config.experimental.list_physical_devices(device_type='GPU')
+cpus = tf.config.experimental.list_physical_devices(device_type='CPU')
+#设置GPU使用策略为需要时申请,以改变默认占据全部内存
+for gpu in gpus:
+    tf.config.experimental.set_memory_growth(device=gpu,enable=True)
+
+#tf.config.experimental.set_visible_devices(devices=gpus[0],device_type='GPU')
+#tf.config.experimental.set_visible_devices(devices=cpus,device_type='CPU')
+
+#超参数设置
+source_domain_path = "f:/pythonwork/DAN/PACS_dataset/kfold/cartoon/"
+target_domain_path = "f:/pythonwork/DAN/PACS_dataset/kfold/photo/"
+checkpoint_path = "f:/pythonwork/DAN/save"
+tb_log_dir = 'f:/pythonwork/DAN/tensorboard'
+
+learning_rate = 0.01
+batch_size = 100
+n_epoch = 2
+lamda = np.array([1,1,1])  #作者实验给出
+
+optimizer = tf.keras.optimizers.Adam(learning_rate= learning_rate)
 
 #log
+current_path = os.path.abspath('.')
 logger = logging.getLogger(__name__)
 logger.setLevel(level=logging.DEBUG)
 
 #Handler
 #file
-handler = logging.FileHandler('f:/pythonwork/Transfer-Learning/dan.log')
+#把log文件存在当前路径下
+log_path = os.path.join(current_path,'log_dan.log')
+handler = logging.FileHandler(log_path)
 handler.setLevel(logging.INFO)
 formatter = logging.Formatter('%(asctime)s-%(name)s-%(levelname)s-%(message)s')
 handler.setFormatter(formatter)
@@ -25,7 +55,7 @@ formatter = logging.Formatter('%(asctime)s-%(message)s')
 handler.setFormatter(formatter)
 logger.addHandler(handler)
 #
-logger.info("this is log")
+logger.info("----------------------this is log----------------")
 
 #自适应层
 class AdaptiveLayer(tf.keras.layers.Layer):
@@ -131,7 +161,7 @@ class DAN(tf.keras.Model):
     def __init__(self):
         super().__init__()
         #原AlexNet框架
-        """         
+        """          
         self.conv1 = tf.keras.layers.Conv2D(activation=tf.nn.relu,
                                         filters=96,
                                         kernel_size = [11,11],
@@ -166,37 +196,38 @@ class DAN(tf.keras.Model):
         """
 
         #较少参数
+                 
         self.conv1 = tf.keras.layers.Conv2D(activation=tf.nn.relu,
-                                        filters=6,
+                                        filters=36,
                                         kernel_size = [3,3],
                                         padding='same',
                                         strides=4)
         self.pool1 = tf.keras.layers.MaxPool2D(pool_size=[3,3],strides=2,padding='valid') 
         self.conv2 = tf.keras.layers.Conv2D(activation=tf.nn.relu,
-                                        filters=6,
-                                        kernel_size = [5,5],
-                                        padding='same',
-                                        strides=1)
-        self.pool2 = tf.keras.layers.MaxPool2D(pool_size=[3,3],strides=2,padding='valid')
-        self.conv3 = tf.keras.layers.Conv2D(activation=tf.nn.relu,
-                                        filters=4,
+                                        filters=36,
                                         kernel_size = [3,3],
                                         padding='same',
-                                        strides=1)
+                                        strides=2)
+        self.pool2 = tf.keras.layers.MaxPool2D(pool_size=[3,3],strides=2,padding='valid')
+        self.conv3 = tf.keras.layers.Conv2D(activation=tf.nn.relu,
+                                        filters=18,
+                                        kernel_size = [3,3],
+                                        padding='same',
+                                        strides=2)
         self.conv4 = tf.keras.layers.Conv2D(activation=tf.nn.relu,
-                                        filters=4,
+                                        filters=9,
                                         kernel_size = [3,3],
                                         padding='same',
                                         strides=1)
         self.conv5 = tf.keras.layers.Conv2D(activation=tf.nn.relu,
-                                        filters=2,
+                                        filters=5,
                                         kernel_size = [3,3],
                                         padding='same',
                                         strides=1)
         self.flatten = tf.keras.layers.Flatten()
         self.dense_ada8 = AdaptiveLayer(units=36,activation=tf.nn.relu)
         self.dense_ada9 = AdaptiveLayer(units=36,activation=tf.nn.relu)
-        self.dense_ada10 = AdaptiveLayer(units=7)
+        self.dense_ada10 = AdaptiveLayer(units=7) 
 
         #其它参数
         self.training = True
@@ -235,12 +266,18 @@ class DAN(tf.keras.Model):
 import pathlib
 import random
 
-class DataLoad(object):
+class Data_Path_Load(object):
+    '''
+    从目标文件夹（domain）加载数据path，并创建对应的label.
+    将数据打乱顺序返回。
+    '''
     def __init__(self,domain_path):
         self.domain_path = pathlib.Path(domain_path)
         
     def get_train_test_set(self,split_ratio):
+        '''
 
+        '''
         all_image_paths = list(self.domain_path.glob('*/*'))
         all_image_paths = [str(path) for path in all_image_paths]
         self.image_count = len(all_image_paths)
@@ -253,15 +290,17 @@ class DataLoad(object):
         
         train_paths,train_labels,test_paths,test_labels = \
             self.shuffle_and_split_path_label(all_image_paths,all_image_labels,split_ratio)
+        """             
         try:
             train_data = list(map(self.load_and_preprocess_image,train_paths))
             test_data = list(map(self.load_and_preprocess_image,test_paths))
         except Exception as e:
             logger.error('Error',exc_info=True)
-            raise e
+            raise e 
+        """
 
 
-        return train_data,train_labels,test_data,test_labels 
+        return train_paths,train_labels,test_paths,test_labels 
 
     #混淆数据顺序
     def shuffle_and_split_path_label(self,all_image_paths,all_image_labels,split_ratio):
@@ -288,7 +327,24 @@ class DataLoad(object):
 
         return train_paths,train_labels,test_paths,test_labels
 
-    #加载图像函数
+#加载图像函数
+class Load_Batch_Image(object):
+    def __init__(self):
+        super().__init__()
+
+    def get_batch_images(self,image_paths):
+        '''
+        return: 返回list,每个分量为tensor
+        '''        
+        try:
+            batch_images = list(map(self.load_and_preprocess_image,image_paths))
+        except Exception as e:
+            logger.error('Error',exc_info=True)
+            raise e 
+
+        return batch_images
+                
+
     def preprocess_image(self,image):
         image = tf.image.decode_jpeg(image,channels=3)
         image = tf.image.resize(image,[227,227])
@@ -296,41 +352,54 @@ class DataLoad(object):
     
         return image
 
-
-
     def load_and_preprocess_image(self,path):
         image = tf.io.read_file(path)
     
         return self.preprocess_image(image)
 #数据处理
-source_path = "f:/Dataset/PACS_dataset/kfold/cartoon/"
-source_load = DataLoad(source_path)
-
-target_path = "f:/Dataset/PACS_dataset/kfold/photo/"
-target_load = DataLoad(source_path)
 
 
+source_load = Data_Path_Load(source_domain_path)
+target_load = Data_Path_Load(target_domain_path)
+#不同定义域训练测试数据分割比例[0,1]
+source_split_ratio = 1.0 
+target_split_ratio = 0.6 
 
-source_data,source_labels,_,_ = source_load.get_train_test_set(split_ratio = 1.0)
-train_data,train_labels,test_data,test_labels = target_load.get_train_test_set(split_ratio = 0.5)
+source_paths,source_labels,_,_ = source_load.get_train_test_set(split_ratio = source_split_ratio)
+train_paths,train_labels,test_paths,test_labels = target_load.get_train_test_set(split_ratio = target_split_ratio)
 
 
 #==============================训练设置=====================================
-learning_rate = 0.01
-batch_size = 50
-n_epoch = 30
-lamda = np.array([0.5,0.5,1.])
-
-optimizer = tf.keras.optimizers.Adam(learning_rate= learning_rate)
 
 model = DAN()
 #accuracy = tf.keras.metrics.SparseCategoricalAccuracy()
+#保存点
+checkpoint = tf.train.Checkpoint(DANmodel = model)
+manager_ckpt = tf.train.CheckpointManager(
+                            checkpoint,
+                            directory=checkpoint_path,
+                            checkpoint_name='dan_model.ckpt',
+                            max_to_keep=3
+                            )
+
+#训练可视化tensorboard
+summary_writer = tf.summary.create_file_writer(tb_log_dir) #保存在当前目录下
+
+
+#依据paths 按批次加载图片
+batch_image_load = Load_Batch_Image()
 
 def shuff_batch(X,y,batch_size):
     #tensor list不支持数组切片的方式，要转成np.array 这步非常耗时
     #X =np.array(X)
     #y =np.array(y)
-
+    #将源域数据划分批次
+    '''
+    生成器
+    args:   X为所有data的paths,
+            y为对应的label
+    return: 混乱后的batch images与对应label
+    '''
     rnd_idx = np.random.permutation(len(X))
     n_batch = len(X)//batch_size
     #for idx_batch in np.array_split(rnd_idx,n_batch):  #第一组竟然会切出batch_size+1个
@@ -339,11 +408,11 @@ def shuff_batch(X,y,batch_size):
         for idx in idx_batch:
             X_batch.append(X[idx])
             y_batch.append(y[idx])
-        #X_batch,y_batch = X[idx_batch],y[idx_batch]
-        yield X_batch,y_batch
+        
+        yield batch_image_load.get_batch_images(X_batch),y_batch
 
 def list_split_even(alist,n_batch,batch_size):
-    #对随机指标按num分割，并保证最后一批长度为偶数
+    #对随机指标按num分割，并保证最后一批长度为偶数(文章中的要求)
     idx_batchs = []
     if (len(alist)%batch_size)%2 == 1:
         alist.pop()
@@ -355,15 +424,23 @@ def list_split_even(alist,n_batch,batch_size):
     return idx_batchs
     
 
-#保证target domian data 可以循环使用
+#保证target domian data 可以循环使用的批次划分,每批和源域数据每批样本量相同。
 class TargetBatch(object):
     def __init__(self,X,y):
+        '''
+        依赖 Load_Batch_image类
+        args:   X为image paths
+                y为对应 label
+        '''
         self.rest_data = X
         self.rest_labels = y
         self.orgin_data = X
         self.orgin_labels = y
 
     def get_batch(self,length):
+        '''
+        return:与length等长的data,label
+        '''
         if len(self.rest_labels)<length:
             self.rest_data = self.orgin_data
             self.rest_labels = self.orgin_labels
@@ -379,9 +456,9 @@ class TargetBatch(object):
             self.rest_labels = self.rest_labels[length:]
 
 
-        return X,y
+        return batch_image_load.get_batch_images(X),y
 
-target_batch = TargetBatch(train_data,train_labels)
+target_batch = TargetBatch(train_paths,train_labels)
 
 loss_object = tf.keras.losses.SparseCategoricalCrossentropy()
 
@@ -394,13 +471,13 @@ test_accuracy = tf.keras.metrics.SparseCategoricalAccuracy(name='test_accuracy')
 #@tf.function
 def train_step(sX_batch,tX_batch,sy_batch):
     #只对部分target data作为有label求交叉熵
-    #当前为无监督，targetdata全部无label
+    #当前为无监督，target data全部无label
 
     st_batch = tf.concat([sX_batch,tX_batch],axis=0)    #将数据拼接并转为tensor
     with tf.GradientTape() as tape:
         output_batch,mmd = model(st_batch)
 
-        pred_batch = output_batch[:len(sy_batch)]
+        pred_batch = output_batch[:len(sy_batch)]        #只用源域上的label 算结构误差
         loss_batch = loss_object(sy_batch,pred_batch)    #自动的将batch中每个的loss加和平均
         loss = loss_batch + np.sum(lamda*mmd)
 
@@ -423,6 +500,9 @@ def test_step(images, labels):
     model.training = True
 
 #开始迭代
+step = 0
+#tf.summary.trace_on(graph=True,profiler=True) # 开启Trace，可以记录图结构和profile信息
+start_time = time.time()
 for epoch in range(n_epoch):
     #每轮清除缓存
     train_loss.reset_states()
@@ -430,15 +510,33 @@ for epoch in range(n_epoch):
     test_loss.reset_states()
     test_accuracy.reset_states()
 
-    for sX_batch,sy_batch in shuff_batch(source_data,source_labels,batch_size):
+    for sX_batch,sy_batch in shuff_batch(source_paths,source_labels,batch_size):
         #保证与当次source batch 内样本数目相同
         tX_batch,ty_batch = target_batch.get_batch(len(sy_batch))
         #执行
         train_step(sX_batch,tX_batch,sy_batch)
         
-    #if epoch%2 == 0:
-        #转到测试模式
-    test_step(test_data,test_labels)
+        step += 1
+        if step%5 == 0:
+            #可视化写入
+            with summary_writer.as_default():
+                tf.summary.scalar('loss',train_loss.result(),step=step)
+                tf.summary.scalar('accuracy',train_accuracy.result(),step=step)
+    """             
+    if epoch == 0:
+        #保存trace
+        with summary_writer.as_default():
+            tf.summary.trace_export(name='DAN_trace',step=step,
+                        profiler_outdir=os.path.join(tb_log_dir,datetime.now().strftime("%Y%m%d-%H%M%S")))
+    """
+
+    if epoch%5 == 0:
+        #保存模型
+        temp_path = manager_ckpt.save(checkpoint_number=epoch)
+        print("model saved to %s"% temp_path)
+
+    """  
+    test_step(batch_image_load.get_batch_images(test_paths),test_labels)
         
     
     template = 'Epoch {}, Loss: {}, Accuracy: {}, Test Loss: {}, Test Accuracy: {}'
@@ -446,9 +544,33 @@ for epoch in range(n_epoch):
                         train_loss.result(),
                         train_accuracy.result()*100,
                         test_loss.result(),
-                        test_accuracy.result()*100))
-        
+                        test_accuracy.result()*100)) 
+    """
+    template = 'Epoch {}, Loss: {},train Accuracy: {}'
+    print(template.format(epoch+1,
+                        train_loss.result(),
+                        train_accuracy.result()*100)
+                        )
 
+#训练完，保存
+temp_path = manager_ckpt.save(checkpoint_number=epoch)
+print("model saved to %s"% temp_path)
 
+logger.info('------------train finish----------------------------')
+
+end_time = time.time()
+print("spend total time:%f"%(end_time-start_time)) 
+logger.info("spend total time:%f"%(end_time-start_time))
+logger.info(template.format(epoch+1,
+                        train_loss.result(),
+                        train_accuracy.result()*100)
+                        )
+""" 
+test_step(batch_image_load.get_batch_images(test_paths),test_labels)
+print('test numbers: {}, test loss: {}, test Accuracy:{}'.format(len(test_labels),
+                                test_loss.result(),
+                                test_accuracy.result()*100
+                                ))
+"""
 logger.info('------------------logging finish---------------------')
 
